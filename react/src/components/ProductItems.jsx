b48ab8cddc59f74e3ce1f903f21112b5fcda12d7
Konrad <konradgrzyb@gmail.com>
2020-09-01 16:49:28 +0200

    Alert dialog while edit or delete product

--- react/src/components/ProductItems.jsx
+++ react/src/components/ProductItems.jsx
@@ -9,16 +9,55 @@ import {
     TextField
 } from "@material-ui/core";
 
+import Dialog from '@material-ui/core/Dialog';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+
 
 
 
 export default function ProductItems({ product, editMode, handleSave, handleDelete, newProduct }) {
+    const [open, setOpen] = React.useState(false);
+    const [openDelete, setOpenDelete] = React.useState(false);
+    const [result, setResult] = React.useState({});
+
+    const handleClickOpen = (data) => {
+        setResult(data);
+        setOpen(true);
+    };
+    
+    const handleClickOpenDelete = (data) => {
+        setOpenDelete(true);
+    };
+
+    const handleClose = () => {
+        setOpen(false);
+    };
+    
+    const handleCloseDelete = () => {
+        setOpenDelete(false);
+    };
+    
+    const handleAgree = () => {
+        handleSave(result);
+        setOpen(false);
+    }
+
+    const handleAgreeDelete = () => {
+        handleDelete();
+        setOpenDelete(false);
+    }
+
     const handleSubmit = e => {
         e.preventDefault();
         const elements = e.target.elements;
-        const result = getResult(elements, product);
-        debugger;
-        handleSave(result);
+        const data = getResult(elements, product);
+
+        handleClickOpen(data);
+        // TODO: show modal 
+        // handleSave(result);
     }
     const config = genSettings(product);
     const style = {
@@ -33,7 +72,9 @@ export default function ProductItems({ product, editMode, handleSave, handleDele
         },
     };
 
-    return (<Grid container spacing={2}>
+
+
+    return (<div><Grid container spacing={2}>
         <Grid item xs={12} md={8}>
             <form id="product-form" onSubmit={handleSubmit}>
                 <List dense={true}>
@@ -60,10 +101,58 @@ export default function ProductItems({ product, editMode, handleSave, handleDele
                 </List>
                 <ButtonGroup color="primary" aria-label="outlined primary button group">
                     <Button type="submit" disabled={!editMode}>save</Button>
-                    <Button disabled={newProduct} onClick={handleDelete}>delete</Button>
+                    <Button disabled={newProduct} onClick={handleClickOpenDelete}>delete</Button>
                 </ButtonGroup>
             </form>
         </Grid>
-    </Grid>);
+    </Grid>
+        <Dialog
+            open={open}
+            onClose={handleClose}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle id="alert-dialog-title">{"Do you want to save changes?"}</DialogTitle>
+            {/* <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                    Do you want to save changes of edited product? .
+          </DialogContentText>
+            </DialogContent> */}
+            <DialogActions>
+                <Button onClick={handleClose} color="primary">
+                    Disagree
+          </Button>
+                <Button onClick={handleAgree} color="primary" autoFocus>
+                    Agree
+          </Button>
+            </DialogActions>
+        </Dialog>
+
+
+        {/* DELETE DIALOG */}
+        <Dialog
+            open={openDelete}
+            onClose={handleCloseDelete}
+            aria-labelledby="alert-dialog-title"
+            aria-describedby="alert-dialog-description"
+        >
+            <DialogTitle id="alert-dialog-title">{"Do you want to delete this product?"}</DialogTitle>
+            {/* <DialogContent>
+                <DialogContentText id="alert-dialog-description">
+                    Do you want to save changes of edited product? .
+          </DialogContentText>
+            </DialogContent> */}
+            <DialogActions>
+                <Button onClick={handleCloseDelete} color="primary">
+                    Disagree
+          </Button>
+                <Button onClick={handleAgreeDelete} color="primary" autoFocus>
+                    Agree
+          </Button>
+            </DialogActions>
+        </Dialog>
+
+    
+    </div>);
 }
 
