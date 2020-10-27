07be383e6e21bf4d51c34c31a6dc0179835e65b2
Konrad GRZYB <konrad.grzyb@alten.com>
2020-10-27 11:27:10 +0100

    Server and routes set for XSUAA

--- server/routes.js
+++ server/routes.js
@@ -10,28 +10,37 @@ router.get("/", (req, res) => {
     res.status(200).sendFile(file);
 });
 
-router.get("/token", (req, res) => {
-    // token will be in response header
-    let msg = { success: false };
-    if (req.authInfo) {
-        msg = { success: true };
-    }
-    res.status(200).json(msg);
-});
+// router.get("/token", (req, res) => {
+//     // token will be in response header
+//     let msg = { success: false };
+//     if (req.authInfo) {
+//         msg = { success: true };
+//     }
+//     res.status(200).json(msg);
+// });
 
 router.get("/user", (req, res) => {
-    const info = req.authInfo || {
-        id: "jan.kowalski@example.com",
-        name: {
-            givenName: "Jan",
-            familyName: "Kowalski",
-        },
-    };
-
-    res.status(200).json(info);
+    // need to return name surname and email
+    if (req.authInfo) { 
+        res.status(200).json({
+            email: req.authInfo.id,
+            name: req.authInfo.name.givenName,
+            surname: req.authInfo.name.familyName
+        });
+    } else { 
+        res.status(501).json('No auth info given');
+        return;
+    }
 });
 
 router.get("/product/:id", (req, res) => {
+    const isAuthorized = req.authInfo.checkScope('$XSAPPNAME.Read');
+    if (!isAuthorized) {
+      res.status(403).json('Forbidden');
+      return;
+    }
+
+
     const id = req.params.id;
 
     Product.aggregate([
@@ -42,14 +51,20 @@ router.get("/product/:id", (req, res) => {
             },
         },
     ])
-        .then((data) => {
-            res.status(200).json(data);
-        })
-        .catch((err) => res.status(400).json("Error: ", err));
+    .then((data) => {
+        res.status(200).json(data);
+    })
+    .catch((err) => res.status(400).json("Error: ", err));
 });
 
 // create product
 router.post("/product", (req, res) => {
+    const isAuthorized = req.authInfo.checkScope('$XSAPPNAME.Create');
+    if (!isAuthorized) {
+      res.status(403).json('Forbidden');
+      return;
+    }
+
     // create id
     const input = req.body.prices ? req.body : convertToSchema(req.body);
     console.log(convertToSchema(req.body));
@@ -72,6 +87,13 @@ router.post("/product", (req, res) => {
 
 //The HTTP PUT request method creates a new resource or replaces a representation of the target resource with the request payload.
 router.put("/product/:id", (req, res) => {
+    const isAuthorized = req.authInfo.checkScope('$XSAPPNAME.Update');
+    if (!isAuthorized) {
+      res.status(403).json('Forbidden');
+      return;
+    }
+
+
     const data = req.body.prices ? req.body : convertToSchema(req.body);
     console.log(convertToSchema(req.body));
     const id = req.params.id;
@@ -90,6 +112,12 @@ router.put("/product/:id", (req, res) => {
 
 // delete product
 router.delete("/product/:id", (req, res) => {
+    const isAuthorized = req.authInfo.checkScope('$XSAPPNAME.Delete');
+    if (!isAuthorized) {
+      res.status(403).json('Forbidden');
+      return;
+    }
+
     const id = req.params.id;
 
     // as there are many products with the same id we need to removeMany by this id
@@ -107,6 +135,12 @@ router.delete("/product/:id", (req, res) => {
 });
 
 router.get("/products", (req, res) => {
+    const isAuthorized = req.authInfo.checkScope('$XSAPPNAME.Read');
+    if (!isAuthorized) {
+      res.status(403).json('Forbidden');
+      return;
+    }
+
     const skip = parseInt(req.query.skip);
     const limit = parseInt(req.query.limit);
     const promiseCount = Product.aggregate([
--- server/server.js
+++ server/server.js
@@ -1,47 +1,21 @@
 const express = require("express");
-const graphqlHTTP = require("express-graphql");
-const products = require("./resources/products.json"); // for local testing without database
-const path = require("path");
 const xsenv = require("@sap/xsenv");
 const xssec = require("@sap/xssec");
 const passport = require("passport"); // to use JWTStrategy a plugin to authenticate requests
-const bodyParser = require("body-parser");
-const cors = require("cors");
-
-/**
- * Mongoose
- */
 const mongoose = require("mongoose");
-const Products = require("./mongoose/Product.model");
-const { group } = require("./mongoose/aggregation");
-/**
- * GraphQl
- */
-const schema = require("./graphql/schema");
-const root = require("./graphql/root");
+const cors = require("cors");
+const routes = require('./routes');
 
-require("dotenv").config(); // to load environment vars from .env file like DB_URL
-
-//****************
-// ENV
-//****************
-const NODE_ENV = process.env.NODE_ENV; // prod or development like in React
-const PORT = process.env.PORT || 5000;
-const DB_URL = process.env.DB_URL || "http://localhost:2701"; // use Atlas db if not localhost
-// TODO: add all apps names best - pass object, add xsuaa service name!
-const WHITE_APP = process.env.WHITE_APP;
-
-// **************
-// APP SETTINGS
-// **************
 const app = express();
-//Parse incoming request bodies in a middleware before your handlers, available under the req.body property.
-// app.use( bodyParser() );  // DEPRACTICATED use direct methods like bodyParser.urlencoded() or bodyParser.json()
+// require("dotenv").config(); // to load environment vars from .env file like DB_URL
 
-const whitelist = ["http://localhost:3000", "http://localhost:5000", WHITE_APP];
-
+ 
+/**
+ * Settings
+ */
 const corsOptions = {
     origin: function (origin, callback) {
+        const whitelist = ["http://localhost:3000", "http://localhost:5000", process.env.WHITE_APP];
         // check index of origin in whitelist, if not -1 there is index for origin
         if (whitelist.indexOf(origin) !== -1 || !origin) {
             // If you do not want to block REST tools or server-to-server requests.
@@ -57,178 +31,59 @@ const corsOptions = {
 };
 app.use(cors(corsOptions));
 
-// *******************************
-// MONGO
-// *******************************
-// mongoose.connect(DB_URL, {
-//     useNewUrlParser: true,
-//     useUnifiedTopology: true,
-//     socketTimeoutMS: 0,
-//     keepAlive: true,
-// });
-mongoose.connect(DB_URL, {
+
+/**
+ * Mongo
+ */
+mongoose.connect(process.env.DB_URL, {
     useNewUrlParser: true,
     useUnifiedTopology: true,
     socketTimeoutMS: 0,
     keepAlive: true,
-}, (err) => {
-    if(!err) {
-        console.log('--- MONGO CONNECTED ---');
-    }
-});
-
-//check connection
-// const conn = mongoose.connection;
-// conn.on("error", (err) => console.log(err)); // check if URL founded
-// conn.on("disconnected", () => console.log("Mongo Disconnected :("));
-// conn.on("connected", () => console.log("Mongo Connected :)"));
-
-// *******************************
-// AUTH
-// *******************************
-if (NODE_ENV === "prod") {
-    const services = xsenv.getServices({
-        xsuaa: {
-            name: "eiti-ser-xsuaa",
-        },
-    });
-
-    console.log("xsenv services: ", services);
-
-    const jwts = new xssec.JWTStrategy(services.xsuaa);
-    passport.use(jwts);
-    app.use(passport.initialize());
-    app.use(passport.authenticate("JWT", { session: false })); //authenticate() is used as route middleware to authenticate requests. here JWT token?
-}
-
-// *******************************
-//  API
-// *******************************
-// default
-app.get("/", (req, res) => {
-    // requested not by UI app
-    const file = path.join(__dirname, "./resources/index.html");
-
-    res.status(200).sendFile(file);
-});
-
-// get Token
-app.get("/token", (req, res) => {
-    // token will be in response header
-    let msg = { "success": false };
-    if (req.authInfo) {
-        msg = { "success": true };
-    }
-    res.status(200).json(msg);
-});
-
-// Return local products do not auth!!
-app.get("/api/v1/local/products", (req, res) => {
-    res.status(200).json(products);
-});
-
-app.get("/api/v1/user", (req, res) => {
-    //If JWT token is present in the request and it is successfully verified, following objects are created:
-    // if on CF profile data in req.authInfo
-    const info = req.authInfo || {
-        id: "jan.kowalski@example.com",
-        name: {
-            givenName: "Jan",
-            familyName: "Kowalski",
-        },
-    };
-
-    res.status(200).json(info);
-});
-
-
-// get first product
-app.get("/api/v1/product", (req, res) => {
-    const id = req.query.id;
-    // you need to aggregate before :/ 
-        Products.aggregate([
-            group,
-            {
-                $match: {
-                    '_id': id
-                }
-            }
-        ])
-            .then((data) => {
-                res.status(200).json(data);
-            })
-            .catch((err) => res.status(400).json("Error: ", err));
-    
-});
-
-// this REST not GraphQL!
-app.get("/api/v1/products", (req, res) => {
-    // NOTE: req.query is always defined. If no query given Number({}) => NaN || 0 => 0
-    const skip = Number(req.query.skip) || 0;
-    const limit = Number(req.query.limit) || 100;
-    // NOTE: check if user have rigths to view products. This is defined in xs-sequrity descriptor of xsuaa service.
-
-    const auth = true;
-
-    if (NODE_ENV === "prod") {
-        // in case if dev env
-        auth = req.authInfo.checkScope("$XSAPPNAME.Display");
-    }
-
-
-    if (auth) {
-        Products.aggregate([
-            group,
-            {
-                $sort: {
-                    price: 1,
-                },
-            },
-            {
-                $skip: skip,
-            },
-            {
-                $limit: limit,
-            },
-        ])
-            .then((data) => {
-                res.status(200).json(data);
-            })
-            .catch((err) => res.status(400).json("Error: ", err));
-    } else {
-        res.status(403).send("Not authorize");
-    }
-}); //get('/api/products'
-
-// this is not REST but GraphQl. All methods are in graphql directory
-app.use(
-    "/api/v1/graphql",
-    graphqlHTTP((req) => {
-        /**
-         * Custom context note: By implementing a custom context building function we access the network request and build  context object, and add currentUser to it. After that resolvers getting called by the GraphQL engine. However by default, the express request is passed as the GraphQL contex.
-         * The 403 header is returned by default when GraphQL requests are not authenticated
-         */
-        const auth = true;
-
-        if (NODE_ENV === "prod") {
-            // in case if dev env
-            auth = req.authInfo.checkScope("$XSAPPNAME.Display");
-        }
-
-        if (auth) {
-            return {
-                schema: schema,
-                rootValue: root,
-                graphiql: NODE_ENV === "prod" ? false : true, // do not use graphiql on prod
-                customFormatErrorFn: (error) => ({
-                    message: error.message,
-                    // details: config.isProduction ? null : error.stack
-                }),
-            };
+}, function(err) {
+        if (err) {
+            console.log("**Error** while connecting to MongoDB: ", err);
         } else {
-            res.status(404).send("Forbidden");
+            console.log("**Info**  Connected to MongoDB!");
         }
-    })
+    }
 );
 
-app.listen(PORT, () => console.log(`--- EXPRESS SERVER RUN ON PORT: ${PORT} ---`));
+
+
+/**
+ * Auth
+ */
+const services = xsenv.getServices({
+    xsuaa: {
+        name: "eiti-ser-xsuaa",
+    },
+});
+
+console.log("xsenv services: ", services);
+
+const jwts = new xssec.JWTStrategy(services.xsuaa);
+passport.use(jwts);
+app.use(passport.initialize());
+app.use(passport.authenticate("JWT", { session: false })); //authenticate() is used as route middleware to authenticate requests. here JWT token?
+
+
+
+/**
+ * Routing
+ */
+
+app.use('/', routes);
+app.use('/user', routes);
+app.use('/token', routes);
+app.use('/graphql', routes);
+app.use('/product', routes);
+app.use('/products', routes);
+
+
+
+const PORT = process.env.PORT || '5000';
+// const PORT = '5000';
+app.listen(PORT, () =>
+    console.log(`**Info**  Express server running on port ${PORT}`)
+);
